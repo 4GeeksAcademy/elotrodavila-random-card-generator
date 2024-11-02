04d2c5fa5beec9b691437450ad90555082753426
Federico Davila <30817202+elotrodavila@users.noreply.github.com>
2024-11-02 01:36:49 +0000

    finished the exercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardSuit = ["♥", "♠", "♣", "♦"];
+  const cardValue = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  function generateRandomCard() {
+    let suit = getRandomElement(cardSuit);
+    let value = getRandomElement(cardValue);
+
+    document.getElementById("suit-top").textContent = suit;
+    document.getElementById("suit-bottom").textContent = suit;
+    document.getElementById("number").textContent = value;
+
+    const color = suit == "♥" || suit == "♦" ? "red" : "black";
+    document.getElementById("suit-top").style.color = color;
+    document.getElementById("suit-bottom").style.color = color;
+    document.getElementById("number").style.color = color;
+  }
+
+  generateRandomCard();
 };
